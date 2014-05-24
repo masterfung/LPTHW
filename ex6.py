40a94b974878b8a1c563ff98310e5d4d4cf13354
Tsung Hung <hungmasterj@gmail.com>
2014-05-24 19:46:30 -0400

    more lively updates

---
+++ ex6.py
@@ -0,0 +1,8 @@
+classes = 'In this world there are %d types of classes.' % 2
+mutants = 'mutants'
+humans = 'regular humans'
+special = 'X-men'
+cool = 'The two kinds are: %s and %s but the %s are special. They are also called: %s.' % (mutants, humans, mutants, special)
+
+print classes
+print cool
---
+++ ex7.py
@@ -0,0 +1,13 @@
+print 'There are large giants in this world'
+print 'And they love to eat %s' % 'veggies'
+print 'They can eat anywhere from %s to %s' % (20, 200)
+print '-' * 20
+
+simple1 = 'There'
+simple2 = 'are'
+simple3 = 3
+simple4 = 4
+
+print simple1 + simple2,  #fascinating that the comma caused \
+#the print to be all on one line!
+print simple3 + simple4
