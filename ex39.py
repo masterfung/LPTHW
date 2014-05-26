0e1944e2ae8395b109bdc7227a5defa45db1f1af
Tsung Hung <hungmasterj@gmail.com>
2014-05-26 10:51:50 -0400

    the start

---
+++ ex39.py
@@ -0,0 +1,18 @@
+states = [
+	'Texas': 'TX',
+	'Florida': 'FL',
+	'Rhode Island': 'RI'
+	'New York': 'NY'
+	'California': 'CA'
+	'Illinois': 'IL'
+	]
+
+cities = [
+	'IL': 'Chicago',
+	'CA': 'San Francisco',
+	'NY': 'New York',
+	'RI': 'Providence',
+	'FL': 'Orlando',
+	'TX': 'Austin'
+]
+
