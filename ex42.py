201bb71409004323669c77092112138dd8f940dc
Tsung Hung <hungmasterj@gmail.com>
2014-05-29 00:02:58 -0400

    more exp;

--- ex42.py
+++ ex42.py
@@ -2,20 +2,24 @@ class Animal(object):
 	num_of_legs = 0
 	def __init__(self, name):
 		self._name = name
-	def count_legs(self, count):
+	def count_legs(self, name, count):
+		self._name = name #any way to optimize this?
 		self._count = count
-		print 'Your pet has %s of legs.' % count
+		print 'Your pet, %s, has %s of legs.' % (name, count)
 	def features(self, color, height):
 		self._color = color
 		self._height = height
-		print 'This pet has the natural color of %s and is about this tall: %d' % (color, height)
+		print 'This pet has the natural color of %s and is about this tall: %d cm.' % (color, height)
 
 class Dog(Animal):
 	def bark(self):
 		print 'Woof!'
 
 zeb = Animal('Zeb')
-zeb.count_legs(4)
+zeb.count_legs('Zeb', 4)
+
+happle = Animal('Happle')
+happle.features('red', 62)
 
 class Person(object):
 	def __init__(self, name):
